adc4aaaadf4cab5de23b4460d24658703e460c06
Priyankasaha20 <priyankamay42@gmail.com>
2024-10-28 18:00:47 +0530

    basic functions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 alert("hi");
+
+//for each ,filter,map,indexof
+var arr=[1,2,3,4];
+arr.forEach(function(val){
+   
+});
+var newarr=arr.map(function (val){
+    return 12;
+})
+//map function ek array return krta hai jismein saare elements voh honge jaisa hum chahenge
+
+//arr mei koi bhi change filter kuch change nhi krta
+var ans=arr.filter(function(val){
+    if(val>3){return true;}
+    else{return false;}
+})
+console.log(ans);
+
+//find simply finds if the val is present or not
